b861177a98b9ae7c3b021ac239dea2c6156f757e
Reshi <juancdominici@gmail.com>
2020-08-17 21:30:33 -0300

    Added buttons and script now works with DOM instead of console.logs. Problems with appending childs to the html log of scores. GOTTA FIX CALLBACKS. Maybe change the whole thing

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
-function game(){
-    let counter;
-    
-    for(counter = 0; counter < 5; counter++) {
-        let playerSelection = prompt("Rock, Paper, Scissors?", "Rock");
-        let roundResult = 
-        console.log(singleRound(playerSelection,computerPlay()));
-    }
-}
+const rockbtn = document.getElementById('rock');
+const paperbtn = document.getElementById('paper');
+const scissorsbtn = document.getElementById('scissors');
+let roundCounter = 0;
+
 
 function computerPlay() {
-    let roundMath = Math.floor(Math.random()* 3) - 1;
+    let roundMath = Math.floor(Math.random() * 3) - 1;
     let result;
 
-    if (roundMath === 0){
-        result = "Rock";
-    } else if (roundMath === 1){
-        result = "Paper";
+    if (roundMath === 0) {
+        return result = "Rock";
+    } else if (roundMath === 1) {
+        return result = "Paper";
     } else {
-        result = "Scissors";
+        return result = "Scissors";
     }
-
-    return result;
 }
-    
-function singleRound (playerSelection, computerSelection) {
 
-    let round = (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER")? `You lose! ${computerSelection} beats ${playerSelection}!` :
-    (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS")? `You lose! ${computerSelection} beats ${playerSelection}!`:
-    (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK")? `You lose! ${computerSelection} beats ${playerSelection}!`:
-    (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK")? `You win! ${playerSelection} beats ${computerSelection}!` :
-    (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER")? `You win! ${playerSelection} beats ${computerSelection}!`:
-    (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS")? `You win! ${playerSelection} beats ${computerSelection}!`:
+function singleRound(playerSelection, computerSelection) {
+    let player = playerSelection;
+    let computer = computerSelection;
+    
+    let round = (player === "Rock" && computer === "Paper") ? `You lose! Paper beats Rock!` :
+    (player === "Paper" && computer === "Scissors") ? `You lose! Scissors beats Paper!` :
+    (player === "Scissors" && computer === "Rock") ? `You lose! Rock beats Scissors!` :
+    (player === "Paper" && computer === "Rock") ? `You win! Paper beats Rock!` :
+    (player === "Scissors" && computer === "Paper") ? `You win! Scissors beats Paper!` :
+    (player === "Rock" && computer === "Scissors") ? `You win! Rock beats Scissors!` :
     "It's a tie!";
 
     return round;
 }
 
+function rockRnd(){
+    const log = document.querySelector('#id');
 
+    const content = document.createElement('li');
+    content.textContent = `Round N°${roundCounter}${singleRound("Rock", computerPlay())}`;
+    
+    log.appendChild(content);
+    roundCounter++;
+};
+function paperRnd(){
+    const log = document.querySelector('#id');
 
-game();
+    const content = document.createElement('li');
+    content.textContent = `Round N°${roundCounter}${singleRound("Paper", computerPlay())}`;
 
+    log.appendChild(content);
+    roundCounter++;
+};
+function scissorsRnd(){
+    const log = document.querySelector('#id');
+
+    const content = document.createElement('li');
+    content.textContent = `Round N°${roundCounter} ${singleRound("Scissors", computerPlay())}`;
+
+    log.appendChild(content);
+    roundCounter++;
+} 
+
+// function checkForRoundStart(){
+
+// }
+
+// window.addEventListener("click", () => checkForRoundStart);
+
+rockbtn.addEventListener("click", () => rockRnd());
+paperbtn.addEventListener("click", () => paperRnd());
+scissorsbtn.addEventListener("click", () => scissorsRnd());
 
